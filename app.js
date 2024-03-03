ee5c63018693ae3b53f4a19f0e94cde6cdf5dae4
Vladislove <vladislav.lapko@gmail.com>
2024-03-03 21:03:25 +0100

    Tested app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const Application    = PIXI.Application;
+
+
+const app = new Application({
+    width: 500,
+    height: 500,
+    transparent: false,
+    antialias: true
+});
+
+app.renderer.backgroundColor = 0x23395D;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+
+document.body.appendChild(app.view);
